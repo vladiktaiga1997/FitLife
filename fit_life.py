bbf71f9a88db1e3d02d7899b6036911b8df5b000
Vladislav Astanin <astanin.vlad@yandex.ru>
2026-08-21 15:25:46 +0300

    Исправлены замечания ревьюера

--- fit_life.py
+++ fit_life.py
@@ -1,30 +1,42 @@
 # Проект FitLife - MVP версия 1.0
-print("-" * 40)
+LINE_LENGTH = 40
+STANDARD_RECOMMENDATION = 30
+ML_IN_LITER = 1000
+ROUNDING_DIGITS = 1
+
+print("-" * LINE_LENGTH)
 print("Добро пожаловать в FitLife.")
-print("-" * 40)
+print("-" * LINE_LENGTH)
 
 # 1. Знакомство
 user_name = input("как Вас зовут?")
 user_age = int(input("сколько Вам лет?"))
 
 # 2. Сбор данных
-user_weight = float(input("укажите Ваш вес(в кг)"))
-user_height = float(input("укажите Ваш Рост(в метрах(например, 1.75))"))
+user_weight = float(
+    input("Укажите ваш вес в кг: ").replace(",", ".")
+)
+user_height = float(
+    input(
+        "Укажите ваш рост в метрах, например 1.75: "
+    ).replace(",", ".")
+)
 
-# 3. Логика расчетов (Функции как "черный ящик": используем арифметику)
-# Формула ИМТ: вес разделить на (рост в квадрате)
+# 3. Логика расчетов.
 bmi = user_weight / (user_height ** 2)
-rounding_bmi = round(bmi, 1)
+rounding_bmi = round(bmi, ROUNDING_DIGITS)
 
 
-# Подсчет воды: вес * 30 мл
-water_ml = user_weight * 30
-# 30 - стандартная рекомендация для поддержания водного баланса
-water_l = water_ml / 1000
-rounding_water_l = round(water_l, 1)
+# Подсчет воды.
+water_ml = user_weight * STANDARD_RECOMMENDATION
 
-# 4. Вывод красивого результата
-print(f"Отчёт для пользователя: {user_name}  ({user_age} г.)")
-print(f"Ваш индекс массы тела составляет: {rounding_bmi}")
-print(f"Рекомендуемая норма воды: {rounding_water_l} литров воды в день.")
-print("Расчёт окончен. Будьте здоровы!")
+water_l = water_ml / ML_IN_LITER
+rounding_water_l = round(water_l, ROUNDING_DIGITS)
+
+# 4. Вывод красивого результата.
+print(
+    f"Отчёт для пользователя: {user_name} ({user_age} г.) \n"
+    f"Ваш индекс массы тела составляет: {rounding_bmi}\n"
+    f"Рекомендуемая норма воды: {rounding_water_l} литра воды в день.\n"
+    f"Расчёт окончен. Будьте здоровы!"
+)
